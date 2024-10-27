cda2c80799ef49c64e9fdb6a1cf813acae10a2c0
Soukarsha Dhar <soukarshadhar91@gmail.com>
2024-10-27 11:42:17 +0530

    Fixed: ClickAwayListener returning true for click event happening inside container

--- src/hooks/useClickAwayListener.ts
+++ src/hooks/useClickAwayListener.ts
@@ -14,28 +14,9 @@ const useClickAwayListener = (handleOnClickAwayListener: () => void) => {
   const handleOnClick = (ev: MouseEvent) => {
     if (!ref.current) return;
 
-    if (ref.current === ev.target) {
-      return;
-    }
+    const insideContainerRef = ev.composedPath().includes(ref.current);
 
-    let found = false;
-    const nodes = (ref.current as any).childNodes as NodeList;
-
-    const findTargetInNodeList = (nodeList: NodeList) => {
-      for (let i = 0; i < nodeList.length; i++) {
-        if (nodeList[i] === ev.target) {
-          found = true;
-          break;
-        }
-        const childs = nodeList[i].childNodes;
-        if (childs.length > 0) findTargetInNodeList(childs);
-      }
-      return;
-    };
-
-    findTargetInNodeList(nodes);
-
-    if (!found) handleOnClickAwayListener();
+    if (!insideContainerRef) handleOnClickAwayListener();
   };
 
   return ref;
